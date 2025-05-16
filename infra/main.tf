aafe91a598f23dcb460496b70a85382b201104ba
Uriel C. <urielchoen008@gmail.com>
2025-05-16 18:22:06 +0300

    fix: removing public access to bucket

--- infra/main.tf
+++ infra/main.tf
@@ -10,25 +10,25 @@ resource "aws_s3_bucket_public_access_block" "public" {
   bucket = aws_s3_bucket.resume.id
 
   block_public_acls       = false
-  block_public_policy     = false
+  block_public_policy     = true
   ignore_public_acls      = false
-  restrict_public_buckets = false
+  restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_policy" "resume_policy" {
-  bucket = aws_s3_bucket.resume.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = ["s3:GetObject"],
-        Resource  = "${aws_s3_bucket.resume.arn}/*"
-      }
-    ]
-  })
-}
+# resource "aws_s3_bucket_policy" "resume_policy" {
+#   bucket = aws_s3_bucket.resume.id
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect    = "Allow",
+#         Principal = "*",
+#         Action    = ["s3:GetObject"],
+#         Resource  = "${aws_s3_bucket.resume.arn}/*"
+#       }
+#     ]
+#   })
+# }
 
 resource "aws_s3_object" "resume_file" {
   bucket = aws_s3_bucket.resume.bucket
