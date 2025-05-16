0310cab727d924bc198e026c1d857f32731dd79a
Uriel C. <urielchoen008@gmail.com>
2025-05-16 18:38:20 +0300

    feat: added initial cloudfront terraform.

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,8 @@ provider "aws" {
   region = var.region
 }
 
+# Section: S3 Bucket
+
 resource "aws_s3_bucket" "resume" {
   bucket = var.bucket_name
 }
@@ -15,24 +17,88 @@ resource "aws_s3_bucket_public_access_block" "public" {
   restrict_public_buckets = true
 }
 
-# resource "aws_s3_bucket_policy" "resume_policy" {
-#   bucket = aws_s3_bucket.resume.id
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Effect    = "Allow",
-#         Principal = "*",
-#         Action    = ["s3:GetObject"],
-#         Resource  = "${aws_s3_bucket.resume.arn}/*"
-#       }
-#     ]
-#   })
-# }
-
 resource "aws_s3_object" "resume_file" {
   bucket = aws_s3_bucket.resume.bucket
   key    = "resume.html"
   source = "${path.module}/../frontend/index.html"
   content_type = "text/html"
+}
+
+# Section: Cloudfront
+
+resource "aws_cloudfront_origin_access_control" "resume" {
+  name                              = "resume-cloudfront-oac"
+  description                       = "OAC for private resume html S3 bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# 3. CloudFront Distribution
+locals {
+  s3_origin_id = "S3Origin"
+}
+resource "aws_cloudfront_distribution" "resume" {
+  origin {
+    domain_name = "${aws_s3_bucket.resume.bucket}.s3.amazonaws.com"
+    origin_access_control_id = aws_cloudfront_origin_access_control.resume.id
+    origin_id   = local.s3_origin_id
+  }
+
+  enabled             = true
+  default_root_object = "resume.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+data "aws_iam_policy_document" "cloudfront_access" {
+  statement {
+    sid    = "AllowCloudFrontAccess"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    actions = ["s3:GetObject"]
+    resources = [
+      "${aws_s3_bucket.resume.arn}/*"
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.resume.arn]
+    }
+  }
+}
+
+# Linking the cloudfront access policy to the s3 bucket
+resource "aws_s3_bucket_policy" "resume_policy" {
+  bucket = aws_s3_bucket.resume.id
+  policy = data.aws_iam_policy_document.cloudfront_access.json
 }
