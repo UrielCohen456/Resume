0b7551635dcf7d6ac91f1138350b36aabcdee382
Uriel C. <urielchoen008@gmail.com>
2025-05-17 15:33:50 +0300

    fix: fixed distro name

--- infra/main.tf
+++ infra/main.tf
@@ -160,8 +160,8 @@ resource "aws_route53_record" "root" {
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.resume_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.resume_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.resume.domain_name
+    zone_id                = aws_cloudfront_distribution.resume.hosted_zone_id
     evaluate_target_health = false
   }
 }
